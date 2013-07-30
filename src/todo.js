9192f850f38db243c2bea17be4809f1df1df9cff
Alina Guzman <alinochka8@gmail.com>
2013-07-29 21:31:20 -0400

    Got the complete and delete buttons to work finally

--- src/todo.js
+++ src/todo.js
@@ -1,45 +1,74 @@
-var todoApp = {};
-var todoItem = {};
+var todoApp = {
+    createTask: function(text){
+        var task = clone(todoItem);
+        task.setTaskText(text);
+        todoApp.appendTask(task);
+
+    },
+    getUnfinishedItems: function(){
+        return document.getElementById('todo-items');
+    },
+    getFinishedItems: function() {
+        return document.getElementById('completed-items');
+    },
+    appendTask: function(task){
+        this.getUnfinishedItems().appendChild(task.renderNew());
+    }
+
+};
+var todoItem = {
+    setTaskText: function(text){
+        this.taskName = text;
+    },
+
+    renderNew: function() {
+        var liTag = document.createElement('li');
+        liTag.innerHTML = this.taskName;
+        liTag.appendChild(this.completeButton());
+        liTag.appendChild(this.deleteButton());
+        return liTag;
+    },
+
+    completeButton: function() {
+        var button = document.createElement('button');
+        button.innerText = "Complete Button";
+        button.onclick = function(event) {
+            var button = event.target;
+            var task = todoApp.getUnfinishedItems().removeChild(this.parentNode);
+            button.parentNode.removeChild(button);
+            todoApp.getFinishedItems().appendChild(task);
+        };
+       return button;
+    },
+
+    deleteButton: function() {
+        var button = document.createElement('button');
+        button.innerHTML = "Delete Task";
+        button.onclick = function(event) {
+            var button = event.target;
+            button.parentNode.parentNode.removeChild(this.parentNode)
+        };
+        return button;
+    },
+
+    getTask: function(event) {
+        return event.target.parentNode.parentNode.parentNode;
+    }
+
+};
+
 function clone(todoItem) {
     var Clone = function() {};
     Clone.prototype = todoItem;
     return new Clone();
 }
 
-todoApp.createTask = function(taskFieldInput) {
-     var task = clone(todoItem);
-     //taskFieldInput = document.getElementById("new-task-field").value;
-     todoItem.setTaskText(taskFieldInput);
-     todoApp.appendTask(task);
-};
-
-todoItem.setTaskText = function(input){
-    this.taskName = input;
-};
-
-todoApp.appendTask = function(task) {
-    var olTag = document.getElementById('todo-items');
-    var liTag = document.createElement('li');
-    olTag.appendChild(liTag);
-    liTag.innerText = task.taskName;
-};
 
 window.onload = function(){
     document.getElementById("add-item").onclick = function(event) {
         todoApp.createTask(document.getElementById("new-task-field").value);
     };
+
 };
 
-//Implement the basic add item functionality:
-//    - Use the window.onload event to pick out an 'add-item' button
-//- Add an onclick event handler that will call a todoApp createTask method and input from the 'new-task-field' field as an argument
-//- Implement the todoApp createTask method to:
-//    - create a new instance of a todoItem object
-//- Use the new task objects setTaskText method to set the task text from the argument
-//- call the todoApp appendTask method passing the new task object as an argument
-//- Implement the todoApp appendTask method to:
-//    - check that the new task name isn't blank
-//- call the new task render method that returns a DOM object and append it to the todo-items
-//- Implement the necessary todoItem methods:
-//    - setTaskText method to set the taskName attribute
-//- render method to return an 'li' element with a div for holding the task text
+
